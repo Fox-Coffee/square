ca003f2155114c68e5397986b03a0f7ca3f758f8
Eden Kamieniecki <foxiakkomunista@gmail.com>
2023-11-28 17:34:35 +0100

    Updated the license

---
+++ README.md
@@ -0,0 +1,33 @@
+# WPF Square Calculator
+
+This WPF (Windows Presentation Foundation) application is a simple square calculator. Users can input the side length of a square, and the program calculates and displays the area and perimeter of the square. Additionally, the application allows users to customize the square's appearance, including color, translucency, and visibility.
+
+## Usage
+
+- Launch the application.
+- Enter the side length of the square.
+- Choose a color from the dropdown list.
+- Adjust the checkbox and radio buttons for additional customization.
+- Click the "Draw" button to visualize the square.
+- Click the "Clear" button to reset the input and output fields.
+
+## Prerequisites
+
+- .NET Framework
+
+## Installing
+
+### Build
+
+1. Download the latest release from [here](https://github.com/Fox-Coffee/Square/releases/tag/build).
+2. Unpack the zip file.
+3. Launch the __WpfApp10.exe__ file.
+
+### Source Code
+
+1. Have [Visual Studio](https://visualstudio.microsoft.com/) and the tools necessary to develop WPF applications.
+2. Clone this repository with `git clone https://github.com/fox-coffee/square.git`
+3. Launch the __WpfApp10.sln__ file with the latest version of Visual Studio. ___NOT VISUAL STUDIO CODE___
+
+## License
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE.md) file for details.
---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+     Square: a program used to draw and calculate squares
+    Copyright (C) 2023  Filip Kamieniecki
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace WpfApp10
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void bok_tekst_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                double b = Double.Parse(bok_tekst.Text);
+                if (b > 0)
+                {
+                    pole_tekst.Text = (b * b).ToString();
+                    obwod_tekst.Text = (b * 4).ToString();
+                }
+                else
+                {
+                    pole_tekst.Text = string.Empty;
+                    obwod_tekst.Text = string.Empty;
+                }
+            } catch
+            {
+                pole_tekst.Text = string.Empty;
+                obwod_tekst.Text = string.Empty;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bok_tekst.Text = string.Empty;
+            pole_tekst.Text = string.Empty;
+            obwod_tekst.Text = string.Empty;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                kwadrat.Width = Double.Parse(bok_tekst.Text) * 2;
+                kwadrat.Height = Double.Parse(bok_tekst.Text) * 2;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void show_Checked(object sender, RoutedEventArgs e)
+        {
+            if(check.IsChecked == true)
+            {
+                kwadrat.Opacity = 0.5;
+            }
+            else
+            {
+                kwadrat.Opacity = 1;
+            }
+            check.IsEnabled = true;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            kwadrat.Opacity = 0.5;
+        }
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if(show.IsChecked == true)
+            {
+                kwadrat.Opacity = 1;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            kwadrat.Opacity = 0;
+            check.IsEnabled = false;
+            check.IsChecked = false;
+        }
+    }
+}
